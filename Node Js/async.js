1f30d60e3b7f2da641c3e673394c67c456d63798
Nirali Kanwani <15nhk2000@gmail.com>
2021-05-12 10:57:10 +0530

    async

---
+++ Node Js/async.js
@@ -0,0 +1,30 @@
+//Nested Asynchornous
+const fetchData = () => {
+    const promise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve('Done');
+        },1500);
+    });
+    return promise;
+};
+setTimeout(() => {
+    console.log('Timer is done');
+    fetchData().then(text => {
+        console.log(text);
+        return fetchData();
+    }).then(text2 => {
+        console.log(text2);
+    });
+}, 2000);
+
+//asynchronous does not finish immediately
+// setTimeout(() => {
+//     console.log('Timer is done');
+//     fetchData(text => {
+//         console.log(text);
+//     });
+// }, 2000);
+
+//synchronous
+console.log('Hello');
+console.log('Hie');
