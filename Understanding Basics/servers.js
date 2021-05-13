dd78b9a34fd16d3100c288fb41c2d5e0e23aef76
Nirali Kanwani <15nhk2000@gmail.com>
2021-05-13 10:21:18 +0530

    Delete Understanding Basics directory

--- Understanding Basics/servers.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const routes = require('./routes');
-
-console.log(routes.someText);
-//const server = http.createServer(routes);
-const server = http.createServer(routes.handler);
-
-server.listen(8000);
